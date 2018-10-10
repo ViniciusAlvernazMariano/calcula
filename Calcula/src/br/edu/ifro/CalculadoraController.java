732eea7ab72ff82c8cf7d5e685b8427dccb683e2
Vinícius Alvernaz <vinialvernaz@gmail.com>
2018-10-10 09:56:59 -0400

    efaa

--- Calcula/src/br/edu/ifro/CalculadoraController.java
+++ Calcula/src/br/edu/ifro/CalculadoraController.java
@@ -5,12 +5,19 @@
  */
 package br.edu.ifro;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
@@ -18,18 +25,82 @@ import javafx.scene.control.Label;
  */
 public class CalculadoraController implements Initializable {
     
+    String n1, n2, nR, op;
+            
     @FXML
-    private Label label;
+    private TextField txt1, txt2, txtResult;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private Button btnAdicao, btnSubtracao, btnMultiplicacao, btnDivisao;
+    
+    @FXML
+    private Label lbln1, lbln2;
+    @FXML
+    private Label lblresult;
+    @FXML
+    private Button btnLimpar;
+    @FXML
+    private Button btnHistorico;
+    
+    @FXML
+    private void Adicao(ActionEvent event) {
+        Double N1 = Double.parseDouble(txt1.getText());
+        Double N2 = Double.parseDouble(txt2.getText());
+        Double resultado= N1 + N2;
+        txtResult.setText(resultado.toString());
+        
+        n1 =
+    }
+    @FXML
+    private void Subtracao(ActionEvent event) {
+        Double N1 = Double.parseDouble(txt1.getText());
+        Double N2 = Double.parseDouble(txt2.getText());
+        Double resultado= N1 - N2;
+        txtResult.setText(resultado.toString());
+    }
+    
+    @FXML
+    private void Multiplicacao(ActionEvent event) {
+        Double N1 = Double.parseDouble(txt1.getText());
+        Double N2 = Double.parseDouble(txt2.getText());
+        Double resultado= N1 * N2;
+        txtResult.setText(resultado.toString());
+    }
+    @FXML
+    private void Divisao(ActionEvent event) {
+        Double N1 = Double.parseDouble(txt1.getText());
+        Double N2 = Double.parseDouble(txt2.getText());
+        Double resultado= N1 / N2;
+        txtResult.setText(resultado.toString());
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void Limpar(ActionEvent event) {
+        txt1.setText("");
+        txt2.setText("");
+        txtResult.setText("");
+        
+    }
+
+    @FXML
+    private void Historico(ActionEvent event) {
+         try{
+            FXMLLoader fxmlLoader = new FXMLLoader();
+            fxmlLoader.setLocation(getClass().getResource("/br/edu/ifro/Historico.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(),600,430);
+            Stage stage = new Stage();
+            stage.setTitle("Historico");
+            stage.initStyle(StageStyle.UNDECORATED);
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch(IOException e){
+    }
     
+}
 }
